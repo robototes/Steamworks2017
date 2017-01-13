3413c925b7151a0d54a12f30c8969b8c96ad8a39
s-bowmana <s-bowmana@52S50029943.it.bsd405.org>
2017-01-13 09:57:15 -0800

    Updated README to include description about why the chat program was here. Also, included link to JSON github page.

--- org/json/package-info.java
+++ org/json/package-info.java
@@ -2,7 +2,8 @@
 /**
  * @author not me
  * 
- * JSON java code I found on github. I'll find the link later.
+ * JSON java code I found on github.
+ * https://github.com/stleary/JSON-java
  *
  */
 package org.json;
