341a71ad40c856a236564482d4c94cd14d2f4378
Timothy Eng <timothyeng36@gmail.com>
2017-01-12 17:32:57 -0800

    Added FRCRobot.java (the main class).

---
+++ FRCRobot.java
@@ -0,0 +1,97 @@
+
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+/**
+ * Improved IterativeRobot.
+ * @author Team 2412 <https://robototes.com, github.com/robototes>
+ */
+public class FRCRobot extends IterativeRobot {
+	private boolean firstRun;
+	private boolean startup;
+	
+	public void robotInit() {
+		DriverStation ds = DriverStation.getInstance();
+		Alliance alliance = ds.getAlliance();
+		int location = ds.getLocation();
+		double voltage = ds.getBatteryVoltage();
+		
+		System.out.println("	==== STARTING INITIALIZATION PROCEDURES ====");
+			Timer.delay(0.15);
+		System.out.println("		" + alliance.toString().toUpperCase() + " ALLIANCE, POSITION " + location);
+			Timer.delay(0.2);
+		System.out.println("		RUNNING SYSTEM CHECKS");
+			Timer.delay(0.1);
+		System.out.println("			VOLTAGE: " + voltage);
+			Timer.delay(0.05);
+		if (ds.isFMSAttached()) {
+			System.out.println("			FMS ATTACHED");
+			Timer.delay(0.2);
+		}
+		
+		if (voltage < 12.0) {
+			System.out.println();
+			System.out.println("!!-- WARNING: LOW VOLTAGE --!!");
+			System.out.println();
+		}
+		
+		System.out.println();
+		System.out.println("	=======================");
+		System.out.println("	==== ROBOT IS READY ===");
+		System.out.println("	=======================");
+			
+		startup = true;
+	}
+	
+	public void autonomousInit() {
+		System.out.println("	==== STARTING AUTONOMOUS MODE ====");
+		firstRun = true;
+	}
+	
+	public void autonomousPeriodic() {
+		if (firstRun) {
+			System.out.println("		No autonomous code.");
+			firstRun = false;
+		}
+		
+		Timer.delay(0.05);
+	}
+	
+	public void teleopInit() {
+		System.out.println("	==== STARTING TELE-OPERATED MODE ====");
+		firstRun = true;
+	}
+	
+	public void teleopPeriodic() {
+		if (firstRun) {
+			System.out.println("		No tele-operated code.");
+			firstRun = false;
+		}
+		Timer.delay(0.05);
+	}
+	
+	public void testInit() {
+		System.out.println("	==== TEST MODE STARTING ====");
+	}
+	
+	public void testPeriodic() {
+		LiveWindow.run();
+		Timer.delay(0.005);
+	}
+	
+	public void disabledInit() {
+		if (!startup) {
+			System.out.println("	==== ROBOT DISABLED ====");
+		} else {
+			startup = false;
+		}
+	}
+	
+	public void disabledPeriodic() {
+		Timer.delay(0.1);
+	}
+}
