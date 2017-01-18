2d53fc421843bd8d50fd4f7a9d9dbc2d64999222
Robbie Robototes <robototes2412@gmail.com>
2017-01-17 16:12:57 -0800

    Added VisionController class for controlling Vision processing.

---
+++ org/usfirst/frc/team2412/robot/VisionController.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team2412.robot;
+
+public class VisionController implements RobotController {
+
+	public void processTeleop() {
+	}
+
+	public void processAutonomous() {
+	}
+	
+}
