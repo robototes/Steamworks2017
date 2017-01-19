92b5f4d2a8fa7c56b4d074612ce595ea6759262f
Robbie Robototes <robototes2412@gmail.com>
2017-01-18 18:00:38 -0800

    Send values to and from the raspberry pi.

--- org/usfirst/frc/team2412/robot/VisionController.java
+++ org/usfirst/frc/team2412/robot/VisionController.java
@@ -3,6 +3,53 @@ package org.usfirst.frc.team2412.robot;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class VisionController implements RobotController {
+	
+	/* Raspberry Pi side, in python (sorry Alexander)
+		#!/usr/bin/env python3
+		#
+		# This is a NetworkTables client (eg, the DriverStation/coprocessor side).
+		# You need to tell it the IP address of the NetworkTables server (the
+		# robot or simulator).
+		#
+		# This shows how to use a listener to listen for changes in NetworkTables
+		# values. This will print out any changes detected on the SmartDashboard
+		# table.
+		#
+		
+		import sys
+		import time
+		from networktables import NetworkTables
+		
+		# To see messages from networktables, you must setup logging
+		import logging
+		logging.basicConfig(level=logging.DEBUG)
+		
+		#if len(sys.argv) != 2:
+		#    print("Error: specify an IP to connect to!")
+		#    exit(0)
+		
+		#ip = sys.argv[1]
+		ip = "10.24.12.105"
+		
+		NetworkTables.initialize(server=ip)
+		
+		
+		def valueChanged(table, key, value, isNew):
+		    print("valueChanged: key: '%s'; value: %s; isNew: %s" % (key, value, isNew))
+		    table.putNumber('Y', value)
+		
+		def connectionListener(connected, info):
+		    print(info, '; Connected=%s' % connected)
+		
+		
+		NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
+		
+		sd = NetworkTables.getTable("datatable")
+		sd.addTableListener(valueChanged)
+		
+		while True:
+		    time.sleep(1)
+	 */
 	private int x = 0;
 	private NetworkTable table = null;
 	public void processTeleop() {
@@ -10,7 +57,9 @@ public class VisionController implements RobotController {
 			//Initialize NetworkTable
 			table = NetworkTable.getTable("datatable");
 		}
-		table.putNumber("X", x++);
+		table.putNumber("X", x);
+		x+=2;
+		System.out.println(table.getNumber("Y", -1));
 	}
 
 	public void processAutonomous() {
