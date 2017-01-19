1129099673b6a3acd76a71919237d5a2b33aded1
Robbie Robototes <robototes2412@gmail.com>
2017-01-18 16:06:55 -0800

    Added simple VisionController script for sending data through Network tables.

--- org/usfirst/frc/team2412/robot/VisionController.java
+++ org/usfirst/frc/team2412/robot/VisionController.java
@@ -1,8 +1,16 @@
 package org.usfirst.frc.team2412.robot;
 
-public class VisionController implements RobotController {
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
+public class VisionController implements RobotController {
+	private int x = 0;
+	private NetworkTable table = null;
 	public void processTeleop() {
+		if(table == null) {
+			//Initialize NetworkTable
+			table = NetworkTable.getTable("datatable");
+		}
+		table.putNumber("X", x++);
 	}
 
 	public void processAutonomous() {
