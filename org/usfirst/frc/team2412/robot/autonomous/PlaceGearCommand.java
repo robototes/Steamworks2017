0541cbcbd8bc2e27eb8f2dc7a433dcbf7dabe98d
Robbie Robototes <robototes2412@gmail.com>
2017-10-25 07:32:56 -0700

    Don't drop the gear if Drive forward has been selected.

--- org/usfirst/frc/team2412/robot/autonomous/PlaceGearCommand.java
+++ org/usfirst/frc/team2412/robot/autonomous/PlaceGearCommand.java
@@ -1,17 +1,22 @@
 package org.usfirst.frc.team2412.robot.autonomous;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class PlaceGearCommand extends Command2 {
 
 	private DoubleSolenoid upDownGripper;
 	private DoubleSolenoid openCloseGripperL;
 	private DoubleSolenoid openCloseGripperR;
+
+	private NetworkTable pydashboardTable;
 	
-	public PlaceGearCommand(DoubleSolenoid _upDownGripper, DoubleSolenoid _openCloseGripperL, DoubleSolenoid _openCloseGripperR) {
+	public PlaceGearCommand(DoubleSolenoid _upDownGripper, DoubleSolenoid _openCloseGripperL, DoubleSolenoid _openCloseGripperR, NetworkTable _pydashboardTable) {
 		this.upDownGripper = _upDownGripper;
 		this.openCloseGripperL = _openCloseGripperL;
 		this.openCloseGripperR = _openCloseGripperR;
+
+		this.pydashboardTable = _pydashboardTable;
 	}
 	
 	
@@ -26,6 +31,7 @@ public class PlaceGearCommand extends Command2 {
 	 * Called periodically when the command is running.
 	 */
 	public void execute() {
+		if(pydashboardTable.getString("Step1", "Drive forward").equals("Drive forward")) return;
 		upDownGripper.set(DoubleSolenoid.Value.kReverse);
 		openCloseGripperL.set(DoubleSolenoid.Value.kForward);
 		openCloseGripperR.set(DoubleSolenoid.Value.kForward);
